0fa3d95fe1cdc157032663679883054876c16bd4
fred3m <fred.moolekamp@gmail.com>
2022-09-22 09:27:50 -0700

    Add FitPsfObservation

--- scarlet/lite/observation.py
+++ scarlet/lite/observation.py
@@ -149,3 +149,76 @@ class LiteObservation:
             padding=self.padding,
             convolution_mode=self.mode,
         )
+
+
+class FitPsfObservation(LiteObservation):
+    """An observation that fits the PSF used to convolve the model.
+    """
+    def __init__(self, diff_kernel, fft_shape, images, variance, weights, psfs, model_psf=None, noise_rms=None,
+                 bbox=None, padding=3, convolution_mode="fft"):
+        super().__init__(images, variance, weights, psfs, model_psf, noise_rms,
+                 bbox, padding, convolution_mode)
+
+        self.mode = "fft"
+        self.axes = (-2, -1)
+        self.fft_shape = fft_shape
+
+        # Make the DFT of the psf a fittable parameter
+        self._fitKernel = FistaParameter(diff_kernel.fft(fft_shape, self.axes), 1e-2)
+        self._fitKernel.grad = self.grad_fit_kernel
+        self._fitKernel.prox = self.prox_kernel
+
+    def grad_fit_kernel(self, input_grad, kernel, model_fft):
+        # Transform the upstream gradient into k-space
+        grad_fft = scarlet.fft.Fourier(input_grad)
+        grad_fft = grad_fft.fft(self.fft_shape, self.axes)
+        return grad_fft * model_fft
+
+    def prox_kernel(self, kernel, prox_step=0):
+        # No prox for now
+        return kernel
+
+    @property
+    def fitKernel(self):
+        return self._fitKernel.x
+
+    @property
+    def gradFitKernel(self):
+        return self.fitKernel.real - self.fitKernel.imag*1j
+
+    def convolve(self, image, mode=None, grad=False):
+        """Convolve the model into the observed seeing in each band.
+
+        Parameters
+        ----------
+        image: `~numpy.array`
+            The image to convolve
+        mode: `str`
+            The convolution mode to use.
+            This should be "real" or "fft" or `None`,
+            where `None` will use the default `convolution_mode`
+            specified during init.
+        grad: `bool`
+            Whether this is a backward gradient convolution
+            (`grad==True`) or a pure convolution with the PSF.
+        """
+        if grad:
+            kernel = self.gradFitKernel
+        else:
+            kernel = self.fitKernel
+
+        if kernel is None:
+            return image
+
+        assert(mode is None or mode == "fft")
+
+        image = scarlet.fft.Fourier(image)
+        fft = image.fft(self.fft_shape, self.axes)
+
+        result = scarlet.fft.Fourier.from_fft(fft*kernel, self.fft_shape, image.shape, self.axes)
+        return result.image
+
+    def update(self, it, input_grad, model):
+        model = scarlet.fft.Fourier(model[:, ::-1, ::-1])
+        model_fft = model.fft(self.fft_shape, self.axes)
+        self._fitKernel.update(it, input_grad, model_fft)
